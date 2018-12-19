ae5fe194dc495ba9e3080b047385670af40931a5
Ryan Kelly <ryan@rfk.id.au>
2018-12-19 19:29:08 +1100

    feat(parsing): Add binary formats for Record and Message layers.

--- src/crypto.js
+++ src/crypto.js
@@ -2,13 +2,46 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-'use strict'
+'use strict';
 
 //
-// TLS1.3 Key Schedule.
+// Low-level crypto primitives.
 //
-// In this file we will implement the "key schedule" from
-// https://tools.ietf.org/html/rfc8446#section-7.1, which
-// defines how to calculate various keys as the handshake
-// state progreses.
+// We haven't actually implemented any of the crypto yet,
+// but when we do, this is the file where we'll define the basics.
 //
+
+import {
+  assert,
+} from './utils.js';
+
+export const AEAD_SIZE_INFLATION = 4;
+export const HASH_LENGTH = 32;
+
+// Fake crypto for now, just to try out the message layer.
+
+export async function AEADEncrypt(key, iv, seqNum, plaintext, additionalData) {
+  const ciphertext = new Uint8Array(plaintext.byteLength + 4);
+  ciphertext[0] = seqNum >> 24;
+  ciphertext[1] = seqNum >> 16;
+  ciphertext[2] = seqNum >> 8;
+  ciphertext[3] = seqNum;
+  ciphertext.set(plaintext, 4);
+  return ciphertext;
+}
+
+export async function AEADDecrypt(key, iv, seqNum, ciphertext, additionalData) {
+  let foundSeqNum = ciphertext[0];
+  foundSeqNum = foundSeqNum << 8 | ciphertext[1];
+  foundSeqNum = foundSeqNum << 8 | ciphertext[2];
+  foundSeqNum = foundSeqNum << 8 | ciphertext[3];
+  assert(foundSeqNum === seqNum, 'sequence number mismatch');
+  const plaintext = new Uint8Array(ciphertext.buffer, ciphertext.byteOffset + 4, ciphertext.byteLength - 4);
+  return plaintext;
+}
+
+export async function getRandomBytes(size) {
+  const bytes = new Uint8Array(size);
+  crypto.getRandomValues(bytes);
+  return bytes;
+}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-'use strict'
+'use strict';
 
 // The top-level APIs offered by this library are `ClientConnection` and
 // `ServerConnection` classes.  They each take a callback to be used for
 // sending data to the remote peer, and operate like this:
 //
-//    conn = await ClientConnection.create(psk, pskID, async function send_data_to_server(data) {
+//    conn = await ClientConnection.create(psk, pskId, async function send_data_to_server(data) {
 //      // application-specific sending logic here.
 //    })
 //
@@ -42,18 +42,30 @@
 // in which side is expected to send vs receieve during the protocol handshake.
 
 import {
-    assertIsBytes,
-    bytesToHex,
-    hexToBytes,
-    bytesToUtf8,
-    utf8ToBytes
+  assert,
+  assertIsBytes,
+  bytesToHex,
+  hexToBytes,
+  bytesToUtf8,
+  utf8ToBytes,
+  noop,
 } from './utils.js';
 
-import { State_UNINITIALIZED, State_ERROR } from './statemachine/index.js';
-import { ClientState_START } from './statemachine/client.js';
-import { ServerState_START } from './statemachine/server.js';
+import {
+  getRandomBytes
+} from './crypto.js';
 
-import { OutgoingRecordBuffer, IncomingRecordBuffer } from './recordlayer.js'
+import * as STATE from './states.js';
+
+import {
+  readHandshakeMessage,
+} from './messages.js';
+
+import {
+  RecordSender,
+  RecordReceiver,
+  RECORD_TYPE,
+} from './recordlayer.js';
 
 // !!!!!!!
 // !!
@@ -68,115 +80,148 @@ import { OutgoingRecordBuffer, IncomingRecordBuffer } from './recordlayer.js'
 // this mock version, except it won't have "Insecure" in the class name...
 
 class InsecureConnection {
-    constructor(psk, pskID, sendCallback) {
-        this.psk = assertIsBytes(psk)
-        this.pskID = assertIsBytes(pskID)
-        this.sendCallback = sendCallback
-        this._state = new State_UNINITIALIZED(this)
-        this._pendingSendData = []
-        this._incomingRecords = new IncomingRecordBuffer()
-        this._outgoingRecords = new OutgoingRecordBuffer()
-        this._lastPromise = Promise.resolve()
+  constructor(psk, pskId, sendCallback, randomSalt) {
+    this.psk = assertIsBytes(psk);
+    this.pskId = assertIsBytes(pskId);
+    this.sendCallback = sendCallback;
+    this.randomSalt = randomSalt;
+    this._state = new STATE.UNINITIALIZED(this);
+    this._pendingApplicationData = [];
+    this._recordSender = new RecordSender();
+    this._recordReceiver = new RecordReceiver();
+    this._lastPromise = Promise.resolve();
+  }
+
+  // Subclasses will override this with some async initialization logic.
+  static async create(psk, pskId, sendCallback, randomSalt = null) {
+    randomSalt = randomSalt === null ? await getRandomBytes(32) : randomSalt;
+    return new this(psk, pskId, sendCallback, randomSalt);
+  }
+
+  // These are the three public API methods that
+  // consumers can use to connunicate over TLS.
+
+  async send(data) {
+    assertIsBytes(data);
+    await this._synchronized(async () => {
+      await this._state.sendApplicationData(data);
+    });
+  }
+
+  async recv(data) {
+    assertIsBytes(data);
+    return await this._synchronized(async () => {
+      const [type, buf] = await this._recordReceiver.recv(data);
+      return await this._dispatchIncomingRecord(type, buf);
+    });
+  }
+
+  async close() {
+    await this._synchronized(async () => {
+      await this._state.close();
+    });
+  }
+
+  // Ensure that async functions execute one at a time,
+  // by waiting for the previous call to `_synchronized()` to complete
+  // before starting a new one.  This helps ensure that we complete
+  // one state-machine transition before starting to do the next.
+
+  _synchronized(cb) {
+    const nextPromise = this._lastPromise.then(() => {
+      return cb();
+    }).catch(async err => {
+      // All errors immediately put us in a terminal "error" state.
+      await this._transition(STATE.ERROR, err);
+      throw err;
+    });
+    // We don't want to hold on to the return value or error,
+    // just synchronize on the fact that it completed.
+    this._lastPromise = nextPromise.then(noop, noop);
+    return nextPromise;
+  }
+
+  // This drives internal transition of the state-machine,
+  // ensuring that the new state is properly initialized.
+
+  async _transition(State, ...args) {
+    this._state = new State(this);
+    await this._state.initialize(...args);
+  }
+
+  // These are helpers to allow the state to add data to the next outgoing record.
+
+  async _writeApplicationData(bytes) {
+    await this._recordSender.withBufferWriter(RECORD_TYPE.APPLICATION_DATA, async buf => {
+      await buf.writeBytes(bytes);
+    });
+  }
+
+  async _writeHandshakeMessage(msg) {
+    await this._recordSender.withBufferWriter(RECORD_TYPE.HANDSHAKE, async buf => {
+      // XXX TODO: remember buffer position here.
+      await msg.write(buf);
+      // XXX TODO: read back to prev buffer position, add it to the transcript hash.
+    });
+  }
+
+  async _flushOutgoingRecord() {
+    const record = await this._recordSender.flush();
+    await this.sendCallback(record);
+  }
+
+  // This is a helper for handling incoming records.
+
+  async _dispatchIncomingRecord(type, buf) {
+    switch (type) {
+      case RECORD_TYPE.ALERT:
+        // XXX TODO: implement alert records for communicating errors.
+        throw new Error('received TLS alert record, aborting!');
+      case RECORD_TYPE.APPLICATION_DATA:
+        return await this._state.recvApplicationData(buf);
+      case RECORD_TYPE.HANDSHAKE:
+        // For simplicity, we assume that handshake messages will not be
+        // fragmented across multiple records.  They shouldn't need to be
+        // for the tiny subset of TLS that we're using.
+        do {
+          // XXX TODO: remember buffer position here.
+          const msg = readHandshakeMessage(buf);
+          // XXX TODO: read back to prev buffer position, add it to the transcript hash.
+          await this._state.recvHandshakeMessage(msg);
+        } while (buf.hasMoreBytes());
+        return null;
+      default:
+        assert(false, `unknown record type: ${type}`);
     }
+  }
 
-    // Subclasses will override this with some async initialization logic.
-    static async create(psk, pskID, sendCallback) {
-        return new this(psk, pskID, sendCallback)
-    }
+  // This is a placeholder, until we implement the full key schedule.
+  // XXX TODO: the full key schedule.
 
-    // These are the three public API methods that
-    // consumers can use to connunicate over TLS.
-
-    async send(data) {
-        assertIsBytes(data)
-        await this._serialize(async () => {
-            this._pendingSendData.push(data)
-            await this._state.send()
-        })
-    }
-
-    async recv(data) {
-        assertIsBytes(data)
-        return await this._serialize(async () => {
-            this._incomingRecords.append(data)
-            return await this._state.recv()
-        })
-    }
-
-    async close() {
-        await this._serialize(async () => {
-            await this._state.close()
-        })
-    }
-
-    // Ensure that async functions execute one at a time,
-    // by waiting for the previous call to `_serialize()` to complete
-    // before starting a new one.  This helps ensure that we complete
-    // one state-machine transition before starting to do the next.
-    //
-    // XXX TODO: this risks keeping the result of the last call around
-    // in memory, we should probably "void" it out somehow on completion.
-
-    _serialize(cb) {
-        this._lastPromise = this._lastPromise.then(() => {
-            return cb();
-        }).catch(async err => {
-            // All errors immediately put us in a terminal "error" state.
-            await this._transition(State_ERROR, err)
-            throw err
-        });
-        return this._lastPromise;
-    }
-
-    // This drives internal transition of the state-machine,
-    // ensuring that the new state is properly initialized.
-
-    async _transition(State, ...args) {
-        this._state = new State(this)
-        await this._state.initialize(...args)
-    }
-
-    // These are some helpers for reading/writing different types of
-    // structured message from the underlying byte arrays.
-
-    async _addOutgoingMessage(typeCls, ...args) {
-        await typeCls.write(this._outgoingRecords, ...args)
-    }
-
-    async _flushOutgoingRecords() {
-        await this.sendCallback(this._outgoingRecords.finalize())
-        this._outgoingRecords = new OutgoingRecordBuffer()
-    }
-
-    _hasIncomingMessage() {
-        return this._incomingRecords.hasMoreBytes()
-    }
-
-    async _getIncomingMessage(typeCls, ...args) {
-        return await typeCls.read(this._incomingRecords, ...args)
-    }
-
+  _updateTrafficKeys() {
+    this._recordSender.setContentKey(this.psk, new Uint8Array(32));
+    this._recordReceiver.setContentKey(this.psk, new Uint8Array(32));
+  }
 }
 
 
 export class InsecureClientConnection extends InsecureConnection {
-    static async create(psk, pskID, sendCallback) {
-        const self = await super.create(psk, pskID, sendCallback)
-        await self._transition(ClientState_START)
-        return self
-    }
+  static async create(psk, pskId, sendCallback) {
+    const instance = await super.create(psk, pskId, sendCallback);
+    await instance._transition(STATE.CLIENT_START);
+    return instance;
+  }
 }
 
 
 export class InsecureServerConnection extends InsecureConnection {
-    static async create(psk, pskID, sendCallback) {
-        const self = await super.create(psk, pskID, sendCallback)
-        await self._transition(ServerState_START)
-        return self
-    }
+  static async create(psk, pskId, sendCallback) {
+    const instance = await super.create(psk, pskId, sendCallback);
+    await instance._transition(STATE.SERVER_START);
+    return instance;
+  }
 }
 
 
 // Re-export helpful utilities for calling code to use.
-
 export { bytesToHex, hexToBytes, bytesToUtf8, utf8ToBytes };
